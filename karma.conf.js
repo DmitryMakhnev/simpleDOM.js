fd6ad81c396f105873ed3af212e45fc809c84fdc
Dmitry Makhnev <dmitry.ivshin@gmail.com>
2015-11-27 12:50:46 +0300

    fix repo name for npm move to tests to PhantomJS

--- karma.conf.js
+++ karma.conf.js
@@ -102,7 +102,7 @@ module.exports = function (config) {
 
         // start these browsers
         // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-        browsers: ['Chrome'],
+        browsers: ['PhantomJS2'],
 
 
         // Continuous Integration mode
